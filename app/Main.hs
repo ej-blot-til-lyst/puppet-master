44f4a671066a7bcdf2f7da5c66a373a1cd1034a4
Christian Gram Kalhauge <christian@kalhauge.dk>
2017-07-30 11:46:43 +0200

    Updated structure

--- app/Main.hs
+++ app/Main.hs
@@ -3,23 +3,24 @@
 
 module Main where
 
+
 -- import Puppetry.Slave
-import Puppetry.WebSocketServer
+import Puppetry.WebSocketServer hiding (update)
 import Data.Text as T
 
 
 type State = ()
-type Action
+data Action
   = MsgReceived T.Text
 
 main :: IO ()
 main = do
   print "Hallo"
-  simpleServer (WebSocketServer update () (MsgReceived) init)
+  simpleServer $
+    WebSocketServer update () (MsgReceived) ()
 
 update action state =
   case action of
     MsgReceived message ->
       (state, Broadcast $ T.pack ("Nemlig " ++ (T.unpack message)))
 
-init = ()
